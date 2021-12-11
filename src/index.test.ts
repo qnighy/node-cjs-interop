7dba86c9a82d690e87f86f41a9b38891240fe50e
Masaki Hara <ackie.h.gmai@gmail.com>
2021-12-11 19:18:12 +0900

    Add tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,35 +1,80 @@
 import { describe, expect, it } from "@jest/globals";
-import { transform } from "@babel/core";
+import { transform, TransformOptions } from "@babel/core";
 import plugin from ".";
+
+const defaultOptions: TransformOptions = {
+  presets: [],
+  plugins: [plugin],
+  configFile: false,
+  babelrc: false,
+};
 
 describe("babel-plugin-import-interop-only", () => {
   it("wraps default imports", () => {
-    const result = transform(`import f from "mod";\nconsole.log(f);\n`, {
-      presets: [],
-      plugins: [plugin],
-      configFile: false,
-      babelrc: false,
-    });
+    const result = transform(`import f from "mod";\nconsole.log(f);\n`, defaultOptions);
     expect(result?.code).toMatchSnapshot();
   });
 
   it("wraps named default imports", () => {
-    const result = transform(`import { default as f } from "mod";\nconsole.log(f);\n`, {
-      presets: [],
-      plugins: [plugin],
-      configFile: false,
-      babelrc: false,
-    });
+    const result = transform(`import { default as f } from "mod";\nconsole.log(f);\n`, defaultOptions);
     expect(result?.code).toMatchSnapshot();
   });
 
   it("wraps namespace imports", () => {
-    const result = transform(`import * as M from "mod";\nconsole.log(M);\n`, {
-      presets: [],
-      plugins: [plugin],
-      configFile: false,
-      babelrc: false,
-    });
+    const result = transform(`import * as M from "mod";\nconsole.log(M);\n`, defaultOptions);
     expect(result?.code).toMatchSnapshot();
+  });
+
+  it("doesn't wrap named imports", () => {
+    const result = transform(`import { f } from "mod";\nconsole.log(f);\n`, defaultOptions);
+    expect(result?.code).toMatchSnapshot();
+  });
+
+  describe("with typescript", () => {
+    const options: TransformOptions = {
+      ...defaultOptions,
+      presets: ["@babel/preset-typescript"],
+      filename: "test.ts",
+    };
+
+    it("wraps default imports", () => {
+      const result = transform(`import f from "mod";\nconsole.log(f);\n`, options);
+      expect(result?.code).toMatchSnapshot();
+    });
+
+    it("wraps named default imports", () => {
+      const result = transform(`import { default as f } from "mod";\nconsole.log(f);\n`, options);
+      expect(result?.code).toMatchSnapshot();
+    });
+
+    it("wraps namespace imports", () => {
+      const result = transform(`import * as M from "mod";\nconsole.log(M);\n`, options);
+      expect(result?.code).toMatchSnapshot();
+    });
+
+    it("doesn't wrap named imports", () => {
+      const result = transform(`import { f } from "mod";\nconsole.log(f);\n`, options);
+      expect(result?.code).toMatchSnapshot();
+    });
+
+    it("doesn't wrap type imports (default imports)", () => {
+      const result = transform(`import type f from "mod";\n`, options);
+      expect(result?.code).toMatchSnapshot();
+    });
+
+    it("doesn't wrap type imports (named default imports)", () => {
+      const result = transform(`import type { default as f } from "mod";\n`, options);
+      expect(result?.code).toMatchSnapshot();
+    });
+
+    it("doesn't wrap type imports (named default imports, individual specification)", () => {
+      const result = transform(`import { type default as f } from "mod";\n`, options);
+      expect(result?.code).toMatchSnapshot();
+    });
+
+    it("doesn't wrap type imports (namespace imports)", () => {
+      const result = transform(`import type * as M from "mod";\n`, options);
+      expect(result?.code).toMatchSnapshot();
+    });
   });
 })
