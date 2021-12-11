fc7aa4c5cc00718c749a453b1d268a49266beae3
Masaki Hara <ackie.h.gmai@gmail.com>
2021-12-11 19:08:58 +0900

    Do a core implementation

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,35 @@
 import { describe, expect, it } from "@jest/globals";
+import { transform } from "@babel/core";
+import plugin from ".";
 
-describe("something", () => {
-  it("something", () => {
-    expect(42).toBe(42);
+describe("babel-plugin-import-interop-only", () => {
+  it("wraps default imports", () => {
+    const result = transform(`import f from "mod";\nconsole.log(f);\n`, {
+      presets: [],
+      plugins: [plugin],
+      configFile: false,
+      babelrc: false,
+    });
+    expect(result?.code).toMatchSnapshot();
+  });
+
+  it("wraps named default imports", () => {
+    const result = transform(`import { default as f } from "mod";\nconsole.log(f);\n`, {
+      presets: [],
+      plugins: [plugin],
+      configFile: false,
+      babelrc: false,
+    });
+    expect(result?.code).toMatchSnapshot();
+  });
+
+  it("wraps namespace imports", () => {
+    const result = transform(`import * as M from "mod";\nconsole.log(M);\n`, {
+      presets: [],
+      plugins: [plugin],
+      configFile: false,
+      babelrc: false,
+    });
+    expect(result?.code).toMatchSnapshot();
   });
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,52 @@
-export {};
+/// <reference types="@babel/helper-plugin-utils" />
+
+import type * as Babel from "@babel/core";
+import type { Identifier, ImportDeclaration, Program, StringLiteral } from "@babel/types";
+import { declare } from "@babel/helper-plugin-utils";
+
+export default declare<{}, Babel.PluginObj>((api) => {
+  return {
+    visitor: {
+      ImportDeclaration(path) {
+        if (path.node.importKind === "type") return;
+        for (const specifier of path.get("specifiers")) {
+          if (specifier.isImportDefaultSpecifier()) {
+            wrapImportInterop(api.types, path, specifier.node.local.name, false);
+          } else if (specifier.isImportSpecifier()) {
+            if (importName(specifier.node.imported) !== "default") return;
+            if (specifier.node.importKind === "type") continue;
+            wrapImportInterop(api.types, path, specifier.node.local.name, false);
+          } else if (specifier.isImportNamespaceSpecifier()) {
+            wrapImportInterop(api.types, path, specifier.node.local.name, true);
+          }
+        }
+      },
+    },
+  };
+});
+
+function wrapImportInterop(t: typeof Babel.types, path: Babel.NodePath<ImportDeclaration>, name: string, wildcard: boolean) {
+  const wrapper = path.scope.generateUidIdentifier(name);
+  const program = path.scope.getProgramParent().path as Babel.NodePath<Program>;
+  const localBinding = program.scope.getBinding(name);
+  const helper: Identifier = path.scope.hub.addHelper(wildcard ? "interopRequireWildcard" : "interopRequireDefault");
+  program.traverse({
+    Identifier(path) {
+      if (!path.isReferencedIdentifier()) return;
+      const referencedBinding = path.scope.getBinding(path.node.name);
+      if (referencedBinding === localBinding || (!referencedBinding && path.node.name === name)) {
+        if (wildcard) {
+          path.replaceWith(t.cloneNode(wrapper));
+        } else {
+          path.replaceWith(t.memberExpression(t.cloneNode(wrapper), t.identifier("default")));
+        }
+      }
+    },
+  });
+  program.unshiftContainer("body", t.variableDeclaration("const", [t.variableDeclarator(wrapper, t.callExpression(t.cloneNode(helper), [t.identifier(name)]))]));
+}
+
+function importName(node: Identifier | StringLiteral) {
+  if (node.type === "StringLiteral") return node.value;
+  return node.name;
+}
