3a828259a86b6c7c0079a3a013f8e87a2cbe354b
Masaki Hara <ackie.h.gmai@gmail.com>
2021-12-11 22:24:12 +0900

    Always use interopRequireDefault

--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,13 @@ export default declare<{}, Babel.PluginObj>((api) => {
         if (path.node.importKind === "type") return;
         for (const specifier of path.get("specifiers")) {
           if (specifier.isImportDefaultSpecifier()) {
-            wrapImportInterop(api.types, path, specifier.node.local.name, false);
+            wrapImportInterop(api.types, path, specifier.node.local.name);
           } else if (specifier.isImportSpecifier()) {
             if (importName(specifier.node.imported) !== "default") return;
             if (specifier.node.importKind === "type") continue;
-            wrapImportInterop(api.types, path, specifier.node.local.name, false);
+            wrapImportInterop(api.types, path, specifier.node.local.name);
           } else if (specifier.isImportNamespaceSpecifier()) {
-            wrapImportInterop(api.types, path, specifier.node.local.name, true);
+            wrapImportInterop(api.types, path, specifier.node.local.name);
           }
         }
       },
@@ -25,21 +25,17 @@ export default declare<{}, Babel.PluginObj>((api) => {
   };
 });
 
-function wrapImportInterop(t: typeof Babel.types, path: Babel.NodePath<ImportDeclaration>, name: string, wildcard: boolean) {
+function wrapImportInterop(t: typeof Babel.types, path: Babel.NodePath<ImportDeclaration>, name: string) {
   const wrapper = path.scope.generateUidIdentifier(name);
   const program = path.scope.getProgramParent().path as Babel.NodePath<Program>;
   const localBinding = program.scope.getBinding(name);
-  const helper: Identifier = path.scope.hub.addHelper(wildcard ? "interopRequireWildcard" : "interopRequireDefault");
+  const helper: Identifier = path.scope.hub.addHelper("interopRequireDefault");
   program.traverse({
     Identifier(path) {
       if (!path.isReferencedIdentifier()) return;
       const referencedBinding = path.scope.getBinding(path.node.name);
       if (referencedBinding === localBinding || (!referencedBinding && path.node.name === name)) {
-        if (wildcard) {
-          path.replaceWith(t.cloneNode(wrapper));
-        } else {
-          path.replaceWith(t.memberExpression(t.cloneNode(wrapper), t.identifier("default")));
-        }
+        path.replaceWith(t.memberExpression(t.cloneNode(wrapper), t.identifier("default")));
       }
     },
   });
