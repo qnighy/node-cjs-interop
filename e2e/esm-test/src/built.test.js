5b185ad3edb2ff03f7589a7ca50bc7aec8fa15ed
Masaki Hara <ackie.h.gmai@gmail.com>
2021-12-12 16:15:22 +0900

    Add more e2e tests

---
+++ e2e/esm-test/src/built.test.js
@@ -0,0 +1,48 @@
+import { describe, expect, it } from "@jest/globals";
+import util from "node:util";
+import childProcess from "node:child_process";
+import path from "node:path";
+
+const execFile = util.promisify(childProcess.execFile);
+
+const filename = new URL(import.meta.url).pathname;
+const dirname = path.dirname(filename);
+const root = path.resolve(dirname, "..");
+
+describe("babel-plugin-import-interop-only + babel-cli", () => {
+  it("interoperates with simulated ESM defaults", async () => {
+    const source = path.resolve(root, "dist/test1.js");
+    const { stdout } = await execFile("node", [source]);
+    expect(stdout).toContain("OK");
+  });
+
+  it("interoperates with simulated ESM namespace imports", async () => {
+    const source = path.resolve(root, "dist/test2.js");
+    const { stdout } = await execFile("node", [source]);
+    expect(stdout).toContain("OK");
+  });
+
+  it("interoperates with native ESM defaults", async () => {
+    const source = path.resolve(root, "dist/test3.js");
+    const { stdout } = await execFile("node", [source]);
+    expect(stdout).toContain("OK");
+  });
+
+  it("interoperates with native ESM namespace imports", async () => {
+    const source = path.resolve(root, "dist/test4.js");
+    const { stdout } = await execFile("node", [source]);
+    expect(stdout).toContain("OK");
+  });
+
+  it("interoperates with pure CJS defaults", async () => {
+    const source = path.resolve(root, "dist/test5.js");
+    const { stdout } = await execFile("node", [source]);
+    expect(stdout).toContain("OK");
+  });
+
+  it("interoperates with pure CJS namespace imports", async () => {
+    const source = path.resolve(root, "dist/test6.js");
+    const { stdout } = await execFile("node", [source]);
+    expect(stdout).toContain("OK");
+  });
+});
---
+++ e2e/esm-test/src/jest-defaults.test.js
@@ -0,0 +1,22 @@
+import { describe, expect, it } from "@jest/globals";
+
+import value1, { A as A1 } from "test-pkg1";
+import value2, { A as A2 } from "test-pkg2";
+import value3, { A as A3 } from "test-pkg3";
+
+describe("babel-plugin-import-interop-only + jest", () => {
+  it("interoperates with simulated ESM defaults", async () => {
+    expect(value1).toBe("default-value");
+    expect(A1).toBe("A");
+  });
+
+  it("interoperates with native ESM defaults", async () => {
+    expect(value2).toBe("default-value");
+    expect(A2).toBe("A");
+  });
+
+  it("interoperates with pure CJS defaults", async () => {
+    expect(value3()).toBe("default-function");
+    expect(A3).toBe("A");
+  });
+});
