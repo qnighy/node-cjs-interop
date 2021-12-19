25b2790cb6c3b00eb6ffbfb2e422e2c4ec9f4f12
Masaki Hara <ackie.h.gmai@gmail.com>
2021-12-19 22:40:05 +0900

    Add with-typescript tests

---
+++ packages/babel-plugin-node-cjs-interop/test/fixtures/with-typescript/basic/output.mjs
@@ -0,0 +1,19 @@
+const ns = _interopImportCJSNamespace(_nsOrig2);
+
+const _ns = _interopImportCJSNamespace(_nsOrig);
+
+function _interopImportCJSNamespace(ns) {
+  return ns.default && ns.default.__esModule ? ns.default : ns;
+}
+
+/*#__CJS__*/
+// @ts-nocheck
+// @ts-nocheck
+import * as _nsOrig from "mod";
+
+/*#__CJS__*/
+import * as _nsOrig2 from "mod";
+console.log({
+  x: _ns.x,
+  ns
+});
