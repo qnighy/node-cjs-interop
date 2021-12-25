ccb997279518db6ceafef094b09423aa71f4dfb5
Masaki Hara <ackie.h.gmai@gmail.com>
2021-12-25 12:33:03 +0900

    Add TODO test

---
+++ packages/babel-plugin-node-cjs-interop/test/fixtures/basic/todo-forbidden-rewriting/output.mjs
@@ -0,0 +1,10 @@
+const _ns = _interopImportCJSNamespace(_nsOrig);
+
+function _interopImportCJSNamespace(ns) {
+  return ns.default && ns.default.__esModule ? ns.default : ns;
+}
+
+/*#__CJS__*/
+import * as _nsOrig from "mod";
+v = 42;
+[v] = [42];
